5df67bdb4207930ef06d924d9e90285ff7a96eb5
LuisTheDevMagician <luisthedevmagician@gmail.com>
2026-05-30 12:59:55 -0300

    Um atrt que roda diretamente com o bun instalado no pc que ve o enderco atual e injeta no moodle durante build do docker

---
+++ start.ts
@@ -0,0 +1,45 @@
+import { networkInterfaces } from 'os';
+import { spawn } from 'bun';
+
+function getLanIp(): string {
+  for (const nets of Object.values(networkInterfaces())) {
+    for (const net of nets ?? []) {
+      if (net.family === 'IPv4' && !net.internal) return net.address;
+    }
+  }
+  return 'localhost';
+}
+
+const ip = getLanIp();
+const wwwroot = `http://${ip}:8080`;
+
+console.log(`\x1b[36m[start]\x1b[0m IP detectado: \x1b[32m${ip}\x1b[0m`);
+console.log(`\x1b[36m[start]\x1b[0m Moodle em:    \x1b[32m${wwwroot}\x1b[0m`);
+console.log(`\x1b[36m[start]\x1b[0m Jogo em:      \x1b[32mhttp://${ip}:3000\x1b[0m`);
+console.log('');
+
+const proc = spawn(['docker', 'compose', 'up', '-d'], {
+  env: { ...process.env, MOODLE_WWWROOT: wwwroot },
+  stdout: 'inherit',
+  stderr: 'inherit',
+  cwd: import.meta.dir,
+});
+
+await proc.exited;
+
+// Atualiza wwwroot no config.php caso Moodle já estivesse instalado com IP diferente
+const update = spawn([
+  'docker', 'exec', 'perfil_moodle',
+  'sed', '-i', `s|\\$CFG->wwwroot.*=.*'http://[^']*';|\\$CFG->wwwroot   = '${wwwroot}';|`, '/var/www/html/config.php',
+], { stdout: 'inherit', stderr: 'inherit' });
+
+await update.exited;
+
+const purge = spawn([
+  'docker', 'exec', 'perfil_moodle',
+  'php', '/var/www/html/admin/cli/purge_caches.php',
+], { stdout: 'inherit', stderr: 'inherit' });
+
+await purge.exited;
+
+console.log(`\x1b[32m[start]\x1b[0m Pronto! Acesse http://${ip}:8080`);
